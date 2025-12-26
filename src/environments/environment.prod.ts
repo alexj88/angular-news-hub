196c419960e848492a337d11295e3162371d18ce
Alex Johny <a.johny87@gmail.com>
2025-12-25 22:05:31 -0300

    commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  newsApi: {
+    apiKey: 'ea39b050133947ec8b1fd73034f7685b', // Pode ser diferente da de desenvolvimento
+    apiUrl: 'https://newsapi.org/v2/',
+    defaultCountry: 'br',
+    pageSize: 10
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  newsApi: {
+    apiKey: 'ea39b050133947ec8b1fd73034f7685b', // Substitua pela sua chave
+    apiUrl: 'https://newsapi.org/v2/',
+    defaultCountry: 'br', // Brasil como padrão
+    pageSize: 10         // Quantidade de artigos por página
+  }
+};
